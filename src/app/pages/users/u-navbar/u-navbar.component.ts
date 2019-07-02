8cd003905d8988301b507035d60f4e4be4cfa066
Nethmi96 <wnethmi96@gmail.com>
2019-07-02 21:04:00 +0530

    Minor changes to the user dashboard

--- src/app/pages/users/u-navbar/u-navbar.component.ts
+++ src/app/pages/users/u-navbar/u-navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-u-navbar',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    /*Search Bar*/
+    $(function() {
+      $('a[href="#search"]').on('click', function(event) {
+          event.preventDefault();
+          $('#search').addClass('open');
+          $('#search > form > input[type="search"]').focus();
+      });
+
+      $('#search, #search button.close').on('click keyup', function(event) {
+          if (event.target === this || event.target.className === 'close' || event.keyCode === 27) {
+              $(this).removeClass('open');
+          }
+      });
+  });
+
+    /*Menu Icon*/
+    $(document).ready(function() {
+    $('.sidenav').sidenav();
+  });
+  }
+  // logout
+  logout() {
+    this.router.navigate(['/home']);
   }
 
 }
